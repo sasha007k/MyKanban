3a767ad46077def3f9259c509cd981b229cc2ba1
Andriy Haliv <haliv.andriy99@gmail.com>
2020-03-05 22:07:38 +0200

    created interface IUnitOfWork

--- MyKanban/Domain/IUnitOfWork.cs
+++ MyKanban/Domain/IUnitOfWork.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
 
 namespace Domain
 {
-    public class IUnitOfWork
+    public interface IUnitOfWork
     {
+       // IIssueRepository IssueRepository { get; }
+       // ITeamRepository TeamRepository { get; }
+      //  IUserRepository UserRepository { get; }
+      //  IProjectRepository ProjectRepository { get; }
+
+        Task Commit();
     }
 }
