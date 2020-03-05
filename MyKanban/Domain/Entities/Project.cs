45482a5849702e4b309f08a13c84fb6c350bbfd9
Kryklyvets Oleksandr <sashakryklyvets@gmail.com>
2020-03-05 21:58:04 +0200

    Added some entities

---
+++ MyKanban/Domain/Entities/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Project : IEntityBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ MyKanban/Domain/Entities/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Team : IEntityBase
+    {
+        public int Id { get; set; }
+
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        private Team()
+        {
+
+        }
+    }
+}
