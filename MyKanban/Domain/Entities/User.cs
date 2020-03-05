ef14147e14ff245d6c4ef8778a0ed2672f4e0811
Mishka Sorochinskiy <mishash20161506@gmail.com>
2020-03-05 21:52:17 +0200

    added ientitybase and user entity

---
+++ MyKanban/Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities
+{
+    public class User : IdentityUser, IEntityBase
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int AmountOfDoneIssues { get; set; }
+    }
+}
--- MyKanban/Domain/IRepository.cs
+++ MyKanban/Domain/IRepository.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using Domain.Entities;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Domain
-{
-    public class IRepository
-    {
-    }
-}
+//namespace Domain.Repositories
+//{
+//    public interface IRepository<TEntity, TKey> where TEntity : IEntityBase
+//    {
+//        Task<List<TEntity>> GetAll();
+//        Task AddAsync(TEntity entity);
+//        Task<TEntity> FindByIdAsync(TKey id);
+//        void RemoveById(TEntity entity);
+//        void Update(TEntity entity);
+//    }
