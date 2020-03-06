aff266a7b75b4f4540acb37b896debdf99039f1c
bitcher8 <nikita.saakyan@gmail.com>
2020-03-06 12:38:46 +0200

    add interfaces

---
+++ MyKanban/Domain/IRepositories/IProjectRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Repositories
+{
+    public interface IProjectRepository : IRepository<Project, int>
+    {
+    }
+}
