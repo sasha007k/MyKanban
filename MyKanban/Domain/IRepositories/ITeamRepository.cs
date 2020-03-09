7901ca6f1c319084c646a4c874a00f5a8bd53070
Bohdan <179739@gmail.com>
2020-03-09 15:56:39 +0200

    add two interfaces

---
+++ MyKanban/Domain/IRepositories/ITeamRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Repositories
+{
+    public interface ITeamRepository : IRepository<Team, int>
+    {
+    }
+}
